a68d6f600f9075508643857e8f83fe7d317cf183
Patrick Stählin <pstaehlin@futurelab.ch>
2009-01-26 17:26:26 +0000

    digitalSTROM: - Exclude cmake directories from compilation in eclipse project - Use variables for sysroot in Toolchain file

--- Toolchain-picocom1-arm9.cmake
+++ Toolchain-picocom1-arm9.cmake
@@ -6,18 +6,17 @@ SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
 SET(COMPILER_PREFIX /opt/emlix/picocom1/bin/arm-linux-gnueabi-)
+SET(BOARD_SYSROOT /opt/emlix/picocom1/sysroot/)
+SET(LOCAL_SYSROOT /home/patrick/test/sysroot/)
+
 SET(CMAKE_C_COMPILER "${COMPILER_PREFIX}gcc")
 SET(CAKE_CXX_COMPILER "${COMPILER_PREFIX}g++")
-#SET(CMAKE_LINKER:FILEPATH "${COMPILER_PREFIX}ld")
 
-#SET(CMAKE_RANLIB "${COMPILER_PREFIX}ranlib")
-#SET(CMAKE_AR "${COMPILER_PREFIX}ar")
-
-include_directories(/home/patrick/test/sysroot/include/ /opt/emlix/picocom1/sysroot/include/)
-link_directories(/home/patrick/test/sysroot/lib /opt/emlix/picocom1/sysroot/lib)
+include_directories(${LOCAL_SYSROOT}include/ ${BOARD_SYSROOT}include/)
+link_directories(${LOCAL_SYSROOT}lib ${BOARD_SYSROOT}lib)
 
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH  /opt/emlix/picocom1/sysroot/ /home/patrick/test/sysroot)
+SET(CMAKE_FIND_ROOT_PATH  ${BOARD_SYSROOT} ${LOCAL_SYSROOT})
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
