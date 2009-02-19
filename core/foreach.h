0d9bac7f9afd34051df3dd87c564bbd300feea40
Patrick Stählin <pstaehlin@futurelab.ch>
2009-02-19 16:05:42 +0000

    digitalSTROM: - Missing file

---
+++ core/foreach.h
@@ -0,0 +1,15 @@
+/*
+ * foreach.h
+ *
+ *  Created on: Feb 12, 2009
+ *      Author: patrick
+ */
+
+#ifndef FOREACH_H_
+#define FOREACH_H_
+
+#include <boost/foreach.hpp>
+
+#define foreach BOOST_FOREACH
+
+#endif /* FOREACH_H_ */
