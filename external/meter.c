cd8640d5b1a04b7fba9069dd80fd8eabaa7b042f
Patrick Stählin <pstaehlin@futurelab.ch>
2009-04-07 15:43:07 +0000

    digitalSTROM: - Updates to the fake-meter

--- external/meter.c
+++ external/meter.c
@@ -1,17 +1,22 @@
 #include <pic16f628.h>
 
+// Fake power-meter
+// Transmits fake measurements over RS232 and blinks a LED
+
 typedef unsigned int word;
 word at 0x2007  __CONFIG = _CP_OFF & _WDT_OFF & _BODEN_ON & \
          _XT_OSC & _MCLRE_OFF & \
          _LVP_OFF;
 
-void putch( unsigned char _c ) {
+static void putch( unsigned char _c ) {
   // wait for TX-Queue to get empty
   while( !TXIF )
   { }
 
   TXREG = _c;
 }
+
+static void sleep();
 
 static void initializePorts() {
   CMCON = 0x07; // disable comparators
@@ -36,29 +41,40 @@ static void initializePorts() {
 } // initializePorts
 
 
+#define LEDPORT RB0
+
 void main() {
   unsigned char currentCnt;
   unsigned char vals[3];
   unsigned char tmp;
+  unsigned int meterValue;
+  unsigned char iByte;
+ 
   initializePorts();
-
-  currentCnt = 0;
-  vals[0] = 128;
-  vals[1] = 64;
-  vals[2] = 30;
-  tmp = 0;
-
-  RB1 = 1;
+  meterValue = 0;
   
-  while(1) {
-    RB0 = vals[0] < currentCnt;
-    RB6 = vals[1] < currentCnt;
-    RB7 = vals[2] < currentCnt;
-    RB2 = tmp < currentCnt;
-    currentCnt++;
-    if(currentCnt == 0) {
-      tmp++;
+  while(true) {
+    sleep();
+    LEDPORT = !LEDPORT;
+    iByte = sizeof(meterValue) - 1;
+    while(iByte > 0) {
+      putch((meterValue >> iByte) & 0x00FF);
     }
-  } 
-
+    sleep();
+  }
 } // main
+
+
+static void sleep() {
+  int counter1;
+  int counter2;
+  
+  counter1 = 0xFFFF;  
+  while(counter1 > 0) {
+    counter1--;
+    counter2 = 0xFFFF;
+    while(counter2 > 0) {
+      counter2--;
+    }
+  }
+}
