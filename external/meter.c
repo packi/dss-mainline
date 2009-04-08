b5a0c5a4712614aad5dbdb092249f211c57a6503
Patrick Stählin <pstaehlin@futurelab.ch>
2009-04-08 13:00:28 +0000

    digitalSTROM: - Current version of meter

--- external/meter.c
+++ external/meter.c
@@ -5,12 +5,12 @@
 
 typedef unsigned int word;
 word at 0x2007  __CONFIG = _CP_OFF & _WDT_OFF & _BODEN_ON & \
-         _XT_OSC & _MCLRE_OFF & \
+         /*_XT_OSC */ _INTRC_OSC_NOCLKOUT & _MCLRE_OFF & \
          _LVP_OFF;
 
 static void putch( unsigned char _c ) {
   // wait for TX-Queue to get empty
-  while( !TXIF )
+  while( !TRMT )
   { }
 
   TXREG = _c;
@@ -21,12 +21,20 @@ static void sleep();
 static void initializePorts() {
   CMCON = 0x07; // disable comparators
   PORTA = 0x00;
-  PORTB = 0x00;
+  PORTB = 0x05; // set tx & rb0 to 1
   
-  TRISA = 0x07; // set RA0-2 as outputs
-  TRISB = 0x00;
+  TRISA = 0xFF;
+  TRISB = 0xF2;
 
-  SPBRG=21; // 56000 baud @ 20MHz
+  //SPEN = 1;
+  //CREN = 1;
+  SPBRG=0x19; // 9600 baud @ 20MHz
+  //BRGH = 1;
+  //TXSTA = TXSTA | 0x20;
+
+  TXSTA = 0x24;
+  RCSTA = 0x90;
+  /*
   BRGH=1; // high baud rate
   SYNC=0; // asyncronous mode
   SPEN=1; // enable serial port
@@ -38,43 +46,64 @@ static void initializePorts() {
   RX9=0; // also for receiving
   TXEN=0; // disable transmitter
   TXEN=1; // and re-enable it to reset
+*/
 } // initializePorts
 
 
 #define LEDPORT RB0
 
 void main() {
-  unsigned char currentCnt;
-  unsigned char vals[3];
-  unsigned char tmp;
+  unsigned int tmp;
   unsigned int meterValue;
-  unsigned char iByte;
+  unsigned char out;
+  unsigned char count;
+  unsigned char i;
+  unsigned char buf[7];
+
+
  
   initializePorts();
   meterValue = 0;
-  
-  while(true) {
+  out = 0xFF;
+  count = 0;
+  while(1) {
     sleep();
-    LEDPORT = !LEDPORT;
-    iByte = sizeof(meterValue) - 1;
-    while(iByte > 0) {
-      putch((meterValue >> iByte) & 0x00FF);
+    LEDPORT = out;
+    //putch('A');
+    //putch('B');
+    out = !out;
+    if(!out) {
+      putch('S');
+      tmp = meterValue;
+      i = 0;
+      do {
+        //buf[i] = '0' + tmp % 10;
+        //i++;
+	putch('0' + tmp % 10);	
+      } while( (tmp /= 10) != 0);
+      //while(i > 0) {
+       // putch(buf[i]);
+//	i--;
+  //    }
+      putch('E');
     }
     sleep();
+    count++;
+    if(count % 4 == 0) {
+      meterValue++;
+      count = 0;
+    }
   }
 } // main
 
 
 static void sleep() {
-  int counter1;
-  int counter2;
+  unsigned int counter1;
+  unsigned int counter2;
   
   counter1 = 0xFFFF;  
   while(counter1 > 0) {
     counter1--;
     counter2 = 0xFFFF;
-    while(counter2 > 0) {
-      counter2--;
-    }
   }
 }
