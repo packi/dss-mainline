f491e245174e0749746136e3875915489a943b3d
Patrick Stählin <pstaehlin@futurelab.ch>
2009-07-24 14:32:53 +0000

    Constants for dSLink

--- core/ds485const.h
+++ core/ds485const.h
@@ -109,6 +109,12 @@ namespace dss {
 
   const uint8_t FunctionModulatorGetEnergyBorder = 0x97;
 
+  const uint8_t FunctionDSLinkConfigWrite = 0xa0;
+  const uint8_t FunctionDSLinkConfigRead = 0xa1;
+  const uint8_t FunctionDSLinkSend = 0xa2;
+  const uint8_t FunctionDSLinkReceive = 0xa3;
+  const uint8_t FunctionDSLinkInterrupt = 0x85;
+
   // Scene constants for devices
   const uint8_t SceneOff = 0x00;
   const uint8_t Scene1 = 0x05;
