e8aa9550dd92b69e2a505bba8a4a63204cd37b11
Sergey 'Jin' Bostandzhyan <jin@developer.digitalstrom.org>
2010-03-12 10:56:20 +0100

    Added support for getting all latest meter values
    previous version was only supporting .meters(dsid,dsid,...)
    
    now you can also specify .meters(all) to get all meter values
    
    the result object was also adjusted, the array is now called "values",
    i.e.:
    
    {"ok":true,
     "result":
      {
       "values":
       [
         {"dsid":"3504175fe0000000ffc00010","value":0,"date":"2010-03-12 09:52:21"},
         {"dsid":"3504175fe0000000ffc00013","value":0,"date":"2010-03-12 09:52:22"}
       ]
      }
    }

--- core/web/webserverapi.cpp
+++ core/web/webserverapi.cpp
@@ -366,7 +366,7 @@ namespace dss {
     clsMetering.addMethod("getLatest")
       .withParameter("type", "string", true)
       .withParameter("from", "string", true)
-      .withDocumentation("Returns cached energy meter value (in Wh) or cached power consumption value (in mW). The type parameter defines what should be returned, valid types are 'energy' and 'consumption'. The from parameter follows the set-syntax, currently it only supports: .meters(dsid1,dsid2,...)");
+      .withDocumentation("Returns cached energy meter value (in Wh) or cached power consumption value (in mW). The type parameter defines what should be returned, valid types are 'energy' and 'consumption'. The from parameter follows the set-syntax, currently it supports: .meters(dsid1,dsid2,...) and .meters(all)");
 
     return api;
   } // createRestfulAPI
