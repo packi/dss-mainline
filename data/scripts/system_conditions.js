5bc6a2c44685be788be9bfa45ce7607f39c5c687
Michael Tross <michael.tross@aizo.com>
2012-03-23 09:27:21 +0100

    added backend scripts for common event handling

---
+++ data/scripts/system_conditions.js
@@ -0,0 +1,130 @@
+/*
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2012 digitalSTROM.org, Zurich, Switzerland
+ *  Author: Roman Köhler <roman.koehler@aizo.com>
+ *          Michael Troß <michael.tross@aizo.com>
+ */
+
+function checkCondition(pathToDescription)
+{
+    var oBaseConditionNode = Property.getNode(pathToDescription + '/conditions');
+    if (oBaseConditionNode != null)
+    {
+        var oEnabledNode = oBaseConditionNode.getChild('enabled');
+        if (oEnabledNode != null) {
+            if (oEnabledNode.getValue() == false) {
+                return false;
+            }
+        }
+
+        // all system states must match
+        var oStateNode = oBaseConditionNode.getChild('states');
+        var oSystemStates = Property.getNode('/usr/states');
+        if (oStateNode != null && oSystemStates) {
+            var oStateArrayActual = oSystemStates.getChildren();
+            var oStateArrayCondition = oStateNode.getChildren();
+            for (var iIndex=0; iIndex < oStateArrayCondition.length; iIndex++)        // check only for requested state, other ignored
+            {
+                var fFound = false;
+                for (var jIndex=0; jIndex < oStateArrayActual.length; jIndex++) {
+                    var testStateName = oStateArrayActual[jIndex].getChild('name');
+                    var testStateValue = oStateArrayActual[jIndex].getChild('value');
+                    if (oStateArrayCondition[iIndex].getName() == testStateName.getValue()) { // search for a requested state
+                        fFound = true;                                                 // state found ...
+                        if (oStateArrayCondition[iIndex].getValue() != testStateValue.getValue()) {
+                            return false;
+                        }
+                        break;
+                    }
+                }
+                if (oStateArrayCondition[iIndex].getValue() == true) {                 // state was requested as active
+                    if (fFound == false) {                                             // but not found -> condition failed  
+                        return false;
+                    }
+                }
+            }
+        }
+
+        // at least one of the zone states must match
+        var oZoneNode = oBaseConditionNode.getChild('zone-states');
+        if (oZoneNode != null) {
+            var fMatch = false;
+            var oStateArrayActual = oZoneNode.getChildren();
+            for (var iIndex = 0; i < oStateArrayActual.length; iIndex ++) {
+                var testZoneId = oStateArrayActual[iIndex].getChild('zone');
+                var testGroupId = oStateArrayActual[iIndex].getChild('group');
+                var testSceneId = oStateArrayActual[iIndex].getChild('scene');
+
+                var groupNode = Property.getNode('/apartment/zones/zone' +
+                                    testZoneId + '/groups/group' + testGroupId);
+
+                if (groupNode && (groupNode.getChild('lastCalledScene').getValue() == testSceneId)) {
+                    fMatch = true;
+                    break;
+                }
+            }
+            if (!fMatch) {
+                return false;
+            }
+        }
+
+        var oWeekdayNode = oBaseConditionNode.getChild('weekdays');
+        if (oWeekdayNode != null) {
+            var sWeekdayString = oWeekdayNode.getValue();
+            if (sWeekdayString != null) {
+                var oNow = new Date();
+                var oWeekDayArray = sWeekdayString.split(",");
+                var fFound = false;
+                for (var iIndex = 0; iIndex < oWeekDayArray.length; iIndex++)
+                    if (parseInt(oWeekDayArray[iIndex]) == oNow.getDay()) {
+                        fFound = true;
+                        break;
+                    }
+                if (fFound == false) {
+                    return false;
+                }
+            }
+        }
+
+        var oTimeStartNode = oBaseConditionNode.getChild('time-start');
+        if (oTimeStartNode != null) {
+            var sTime = oTimeStartNode.getValue();
+            var oTimeTemp = sTime.split(':');
+            if (oTimeTemp.length == 3) {
+                var secondsSinceMidnightCondition = (parseInt(oTimeTemp[0]) * 3600) + (parseInt(oTimeTemp[1]) * 60) +  parseInt(oTimeTemp[0]);
+                var oNow = new Date();
+                var secondsSinceMidnightNow = (oNow.getHours() * 3600) + (oNow.getMinutes() * 3600) + (oNow.getSecounds());
+                if (! (secondsSinceMidnightCondition <= secondsSinceMidnightNow)) {
+                    return false;
+                }
+            }
+        }
+
+        var oTimeEndNode = oBaseConditionNode.getChild('time-end');
+        if (oTimeEndNode != null) {
+            var sTime = oTimeEndNode.getValue();
+            var oTimeTemp = sTime.split(':');
+            if (oTimeTemp.length == 3) {
+                var secondsSinceMidnightCondition = (parseInt(oTimeTemp[0]) * 3600) + (parseInt(oTimeTemp[1]) * 60) +  parseInt(oTimeTemp[0]);
+                var oNow = new Date();
+                var secondsSinceMidnightNow = (oNow.getHours() * 3600) + (oNow.getMinutes() * 3600) + (oNow.getSecounds());
+                if (! (secondsSinceMidnightNow <= secondsSinceMidnightCondition)) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
---
+++ data/scripts/system_register_trigger.js
@@ -0,0 +1,82 @@
+/*
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2012 digitalSTROM.org, Zurich, Switzerland
+ *  Author: Roman Köhler <roman.koehler@aizo.com>
+ *          Michael Troß <michael.tross@aizo.com>
+ */
+
+function registerTrigger(tPath, tEventName, tParamObj)
+{
+    var iIDNew = 0;
+    var oOldNode = null;
+    var oBaseNode = Property.getNode('/usr/triggers');
+    if (oBaseNode != null) {
+        var oBaseArray = oBaseNode.getChildren();
+        if (oBaseArray != null) {
+            for (var iIndex = 0; iIndex < oBaseArray.length; iIndex++) {
+                var oEntryNode = oBaseArray[iIndex];
+                var iID = parseInt(oEntryNode.name);
+                if (iID >= iIDNew) {
+                    iIDNew = iID + 1;
+                }
+                var oPath = oEntryNode.getChild('triggerPath');
+                if (oPath != null) {
+                    if (oPath.getValue() == tPath) {
+                        oOldNode = oEntryNode;
+                    }
+                }
+            }
+        }
+    }
+
+    var triggerNode;
+    if (oOldNode == null) {
+        Property.setProperty('/usr/triggers/' + iIDNew + '/id', iIDNew);
+        triggerNode = Property.getNode('/usr/triggers/' + iIDNew);
+    } else {
+        triggerNode = oOldNode;
+    }
+    Property.setProperty(triggerNode.getPath() + '/triggerPath', tPath)
+    Property.setProperty(triggerNode.getPath() + '/relayedEventName', tEventName)
+    if (tParamObj != null) {
+        var tParamString = JSON.stringify(tParamObj);
+        Property.setProperty(triggerNode.getPath() + '/additionalRelayingParameter', tParamString)
+    }
+}
+
+function unregisterTrigger(tPath)
+{
+    var oOldNode = null;
+    var oBaseNode = Property.getNode('/usr/triggers');
+    if (oBaseNode != null) {
+        var oBaseArray = oBaseNode.getChildren();
+        if (oBaseArray != null) {
+            for (var iIndex = 0; iIndex < oBaseArray.length; iIndex++) {
+                var oEntryNode = oBaseArray[iIndex];
+                var oPath = oEntryNode.getChild('triggerPath');
+                if (oPath != null) {
+                    if (oPath.getValue() == tPath) {
+                        oOldNode = oEntryNode;
+                    }
+                }
+            }
+        }
+        if (oOldNode != null) {
+            oBaseNode.removeChild(oOldNode);
+        } else {
+            printf('unregisterTrigger: unknown trigger path: ' + tPath);
+        }
+    }
+}
