packi/dss-mainline

commit 0d603805d714d07139bf4db5ce4adbf129bc9a6c

adapt heating businterface to latest dsm-api changes

src/businterface.h
@@ -108,13 +108,13 @@ namespace dss {
   typedef struct {
     uint8_t ControllerMode;
     uint16_t Kp;
-    uint16_t Ts;
+    uint8_t Ts;
     uint16_t Ti;
     uint16_t Kd;
     uint16_t Imin;
     uint16_t Imax;
-    uint16_t Ymin;
-    uint16_t Ymax;
+    uint8_t Ymin;
+    uint8_t Ymax;
     uint8_t AntiWindUp;
     uint8_t KeepFloorWarm;
     uint16_t SourceZoneId;
@@ -131,7 +131,7 @@ namespace dss {
     uint32_t Yp;
     uint32_t Yi;
     uint32_t Yd;
-    uint16_t Y;
+    uint8_t Y;
     uint8_t AntiWindUp;
   } ZoneHeatingInternalsSpec_t;
 

src/ds485/dsbusinterface.cpp
@@ -955,9 +955,10 @@ namespace dss {
   void DSBusInterface::handleHeatingControllerConfig(uint8_t _errorCode,
       dsuid_t _sourceID, dsuid_t _destinationID,
       uint16_t _ZoneId, uint8_t _ControllerMode,
-      uint16_t _Kp, uint16_t _Ts, uint16_t _Ti, uint16_t _Kd,
-      uint16_t _Imin, uint16_t _Imax, uint16_t _Ymin, uint16_t _Ymax,
-      uint8_t _AntiWindUp, uint8_t _KeepFloorWarm, uint16_t _SourceZoneId, uint8_t _Offset, uint8_t _EmergencyValue) {
+      uint16_t _Kp, uint8_t _Ts, uint16_t _Ti, uint16_t _Kd,
+      uint16_t _Imin, uint16_t _Imax, uint8_t _Ymin, uint8_t _Ymax,
+      uint8_t _AntiWindUp, uint8_t _KeepFloorWarm, uint16_t _SourceZoneId,
+      uint8_t _Offset, uint8_t _ManualValue, uint8_t _EmergencyValue) {
     loginFromCallback();
 
     boost::shared_ptr<ZoneHeatingConfigSpec_t> spec(new ZoneHeatingConfigSpec_t);
@@ -974,6 +975,7 @@ namespace dss {
     spec->KeepFloorWarm = _KeepFloorWarm;
     spec->SourceZoneId = _SourceZoneId;
     spec->Offset = _Offset;
+    spec->ManualValue = _ManualValue;
     spec->EmergencyValue = _EmergencyValue;
 
     ModelEvent* pEvent = new ModelEventWithDSID(ModelEvent::etControllerConfig, _destinationID);
@@ -985,14 +987,15 @@ namespace dss {
   void DSBusInterface::handleHeatingControllerConfigCallback(uint8_t _errorCode, void *_userData,
       dsuid_t _sourceID, dsuid_t _destinationID,
       uint16_t _ZoneId, uint8_t _ControllerMode,
-      uint16_t _Kp, uint16_t _Ts, uint16_t _Ti, uint16_t _Kd,
-      uint16_t _Imin, uint16_t _Imax, uint16_t _Ymin, uint16_t _Ymax,
-      uint8_t _AntiWindUp, uint8_t _KeepFloorWarm, uint16_t _SourceZoneId, uint8_t _Offset, uint8_t _EmergencyValue) {
+      uint16_t _Kp, uint8_t _Ts, uint16_t _Ti, uint16_t _Kd,
+      uint16_t _Imin, uint16_t _Imax, uint8_t _Ymin, uint8_t _Ymax,
+      uint8_t _AntiWindUp, uint8_t _KeepFloorWarm, uint16_t _SourceZoneId,
+      uint8_t _Offset, uint8_t _ManualValue, uint8_t _EmergencyValue) {
     if (_errorCode == 0) {
       static_cast<DSBusInterface*>(_userData)->
           handleHeatingControllerConfig(_errorCode, _sourceID, _destinationID, _ZoneId,
               _ControllerMode, _Kp, _Ts, _Ti, _Kd, _Imin, _Imax, _Ymin, _Ymax,
-              _AntiWindUp, _KeepFloorWarm, _SourceZoneId, _Offset, _EmergencyValue);
+              _AntiWindUp, _KeepFloorWarm, _SourceZoneId, _Offset, _ManualValue, _EmergencyValue);
     }
   } // handleHeatingControllerConfigCallback
 

src/ds485/dsbusinterface.h
@@ -226,15 +226,17 @@ namespace dss {
     void handleHeatingControllerConfig(uint8_t _errorCode,
         dsuid_t _sourceID, dsuid_t _destinationID,
         uint16_t _ZoneId, uint8_t ControllerMode,
-        uint16_t _Kp, uint16_t _Ts, uint16_t _Ti, uint16_t _Kd,
-        uint16_t _Imin, uint16_t _Imax, uint16_t _Ymin, uint16_t _Ymax,
-        uint8_t _AntiWindUp, uint8_t _KeepFloorWarm, uint16_t _SourceZoneId, uint8_t _Offset, uint8_t _EmergencyValue);
+        uint16_t _Kp, uint8_t _Ts, uint16_t _Ti, uint16_t _Kd,
+        uint16_t _Imin, uint16_t _Imax, uint8_t _Ymin, uint8_t _Ymax,
+        uint8_t _AntiWindUp, uint8_t _KeepFloorWarm, uint16_t _SourceZoneId,
+        uint8_t _Offset, uint8_t _ManualValue, uint8_t _EmergencyValue);
     static void handleHeatingControllerConfigCallback(uint8_t _errorCode, void *_userData,
         dsuid_t _sourceID, dsuid_t _destinationID,
         uint16_t _ZoneId, uint8_t _ControllerMode,
-        uint16_t _Kp, uint16_t _Ts, uint16_t _Ti, uint16_t _Kd,
-        uint16_t _Imin, uint16_t _Imax, uint16_t _Ymin, uint16_t _Ymax,
-        uint8_t _AntiWindUp, uint8_t _KeepFloorWarm, uint16_t _SourceZoneId, uint8_t _Offset, uint8_t _EmergencyValue);
+        uint16_t _Kp, uint8_t _Ts, uint16_t _Ti, uint16_t _Kd,
+        uint16_t _Imin, uint16_t _Imax, uint8_t _Ymin, uint8_t _Ymax,
+        uint8_t _AntiWindUp, uint8_t _KeepFloorWarm, uint16_t _SourceZoneId,
+        uint8_t _Offset, uint8_t _ManualValue, uint8_t _EmergencyValue);
     void handleHeatingControllerOperationModes(uint8_t _errorCode,
         dsuid_t _sourceID, dsuid_t _destinationID,
         uint16_t _ZoneId,

src/ds485/dsstructuremodifyingbusinterface.cpp
@@ -221,8 +221,9 @@ namespace dss {
     }
     int ret = ControllerHeating_set_config(m_DSMApiHandle, _dsMeterID, _ZoneID,
         _spec.ControllerMode, _spec.Kp, _spec.Ts, _spec.Ti, _spec.Kd,
-        _spec.Imin, _spec.Imax, _spec.Ymin, _spec.Ymax, _spec.AntiWindUp, _spec.KeepFloorWarm,
-        _spec.SourceZoneId, _spec.Offset, _spec.EmergencyValue);
+        _spec.Imin, _spec.Imax, _spec.Ymin, _spec.Ymax,
+        _spec.AntiWindUp, _spec.KeepFloorWarm, _spec.SourceZoneId,
+        _spec.Offset, _spec.ManualValue, _spec.EmergencyValue);
     DSBusInterface::checkResultCode(ret);
   } // setZoneHeatingConfig
 

src/ds485/dsstructurequerybusinterface.cpp
@@ -546,8 +546,9 @@ namespace dss {
     ZoneHeatingConfigSpec_t result;
     int ret = ControllerHeating_get_config(m_DSMApiHandle, _dsMeterID, _ZoneID,
         &result.ControllerMode, &result.Kp, &result.Ts, &result.Ti, &result.Kd,
-        &result.Imin, &result.Imax, &result.Ymin, &result.Ymax, &result.AntiWindUp, &result.KeepFloorWarm,
-        &result.SourceZoneId, &result.Offset, &result.EmergencyValue);
+        &result.Imin, &result.Imax, &result.Ymin, &result.Ymax,
+        &result.AntiWindUp, &result.KeepFloorWarm, &result.SourceZoneId,
+        &result.Offset, &result.ManualValue, &result.EmergencyValue);
     DSBusInterface::checkResultCode(ret);
     return result;
   } // getZoneHeatingConfig

commit 61f7bb7a2cc9008a1cb46be3248a7887fc1496ed

add forgotten ManualValue field in ZoneHeatingConfigSpec_t
regression from 0d603805d714d07139bf4db5ce4adbf129bc9a6c

src/businterface.h
@@ -120,6 +120,7 @@ namespace dss {
     uint16_t SourceZoneId;
     uint8_t Offset;
     uint8_t EmergencyValue;
+    uint8_t ManualValue;
   } ZoneHeatingConfigSpec_t;
 
   typedef struct {