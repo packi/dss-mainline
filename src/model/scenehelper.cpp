a22e57894be7ceec990519eeb323071e61fbe279
Michael Tross <michael.tross@digitalstrom.com>
2014-09-22 18:34:16 +0200

    sensor conversion table: fix typo

--- src/model/scenehelper.cpp
+++ src/model/scenehelper.cpp
@@ -386,7 +386,7 @@ namespace dss {
     case SensorIDBrightnessIndoors:
     case SensorIDBrightnessOutdoors:
     case SensorIDCO2Concentration:
-      convertedSensorValue = (double) (pow(10, ((double) convertedSensorValue) / 800));
+      convertedSensorValue = (double) (pow(10, ((double) _sensorValue) / 800));
       break;
     case SensorIDHumidityIndoors:
     case SensorIDHumidityOutdoors:
