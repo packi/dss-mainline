a9118fffa11d66f6f31665aaefb5c43675d2e35f
Sergey 'Jin' Bostandzhyan <jin@developer.digitalstrom.org>
2015-05-12 20:25:14 +0200

    Added new JSON calls for configuring output after impulse
    /json/device/setOutputAfterImpulse?dsuid=xyz&output=(on|off|retain)
    /json/device/getOutputAfterImpulse

--- src/model/modelconst.h
+++ src/model/modelconst.h
@@ -68,6 +68,7 @@ namespace dss {
   const uint8_t SceneOffE4 = 0x26;
   const uint8_t SceneOnE4 = 0x27;
   const uint8_t SceneAutoOff = 0x28;
+  const uint8_t SceneImpulse = 0x29;
   const uint8_t SceneDecA1 = 0x2A;
   const uint8_t SceneIncA1 = 0x2B;
   const uint8_t SceneDecA2 = 0x2C;
