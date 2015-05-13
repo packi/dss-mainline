packi/dss-mainline

commit 75adf9ab41a8ddec26bbcae2e7aeabb878e6eb50

UMR: reset buttonInputMode to STANDARD when changing color from Joker
On a "second dsid", where no 2way configuration setting is possible,
the buttonInputMode would not be reset when switching from BinarySensor
mode to ButtonMode mode.

This change will potentially lead to an "unnecessary" parameter write to
the terminal block when the real buttonInputMode is written directly after
changing the color.

refs #9395

src/structuremanipulator.cpp
@@ -657,6 +657,13 @@ namespace dss {
     if (oldGroupId != newGroup->getID()) {
       device->setDeviceJokerGroup(newGroup->getID());
       modified = true;
+
+      if ((newGroup->getID() != ColorIDBlack) &&
+          device->hasInput() &&
+          (device->getButtonInputMode() != DEV_PARAM_BUTTONINPUT_STANDARD)) {
+        device->setDeviceButtonInputMode(DEV_PARAM_BUTTONINPUT_STANDARD);
+        device->setButtonInputMode(DEV_PARAM_BUTTONINPUT_STANDARD);
+      }
     }
 
     /* check if device is also in a colored user group */

commit d62488d80137311b81720cbe6d43ca7f1b644641

setJokerGroup: only reset buttonInputMode when switching from Joker
regression from 75adf9ab41a8ddec26bbcae2e7aeabb878e6eb50

src/structuremanipulator.cpp
@@ -568,7 +568,8 @@ namespace dss {
       device->setDeviceJokerGroup(newGroup->getID());
       modified = true;
 
-      if ((newGroup->getID() != ColorIDBlack) &&
+      if ((oldGroupId == ColorIDBlack) &&
+          (newGroup->getID() != ColorIDBlack) &&
           device->hasInput() &&
           (device->getButtonInputMode() != DEV_PARAM_BUTTONINPUT_STANDARD)) {
         device->setDeviceButtonInputMode(DEV_PARAM_BUTTONINPUT_STANDARD);