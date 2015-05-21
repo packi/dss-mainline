90a4f0bed39f51e6b6db21757912f3cae7179b7f
remymahler <remy.mahler@digitalstrom.com>
2015-05-21 13:59:48 +0200

    Synchronize DS Meter data when a device is discovered
    The DS Meter data must be synchronized
    when the ds485 devices are discovered.

--- src/model/busscanner.h
+++ src/model/busscanner.h
@@ -48,6 +48,7 @@ namespace dss {
     bool scanDSMeter(boost::shared_ptr<DSMeter> _dsMeter);
     bool scanDeviceOnBus(boost::shared_ptr<DSMeter> _dsMeter, devid_t _shortAddress);
     void syncBinaryInputStates(boost::shared_ptr<DSMeter> _dsMeter, boost::shared_ptr<Device> _device);
+    void synchronizeDSMeterData(boost::shared_ptr<DSMeter> _dsMeter, DSMeterSpec_t &_spec);
   private:
     bool scanZone(boost::shared_ptr<DSMeter> _dsMeter, boost::shared_ptr<Zone> _zone);
     bool scanClusters(boost::shared_ptr<DSMeter> _dsMeter);
