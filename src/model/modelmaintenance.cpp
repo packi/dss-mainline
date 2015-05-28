8f07e16ac0a6cc30e7e486aaf0ec1c90a546588f
Andreas Fenkart <andreas.fenkart@dev.digitalstrom.org>
2015-05-28 11:24:24 +0200

    Revert "model: main: debug: m_ModelEvent shouldn't be empty if wait returns"
    This reverts commit b6ab8a48763accec0146d6821e213d20bd9c01a6.

--- src/model/modelmaintenance.cpp
+++ src/model/modelmaintenance.cpp
@@ -150,7 +150,6 @@ namespace dss {
   : ThreadedSubsystem(_pDSS, "Apartment"),
     m_IsInitializing(true),
     m_IsDirty(false),
-    m_ModelEventsCounter(0),
     m_pApartment(NULL),
     m_pMetering(NULL),
     m_EventTimeoutMS(_eventTimeoutMS),
@@ -437,10 +436,11 @@ namespace dss {
           }
       }
 
-      log("@wait: m_ModelEvents.size: " + intToString(m_ModelEvents.size()) +
-          " ct: " + intToString(m_ModelEventsCounter) +
-          " did wait: " + intToString(do_wait) +
-          " no timeout: " + intToString(stat == boost::cv_status::no_timeout), lsWarning);
+      if (m_ModelEvents.empty()) { //< debug _taw_
+        log("@wait: m_ModelEvents.size: " + intToString(m_ModelEvents.size()) +
+            "did wait: " + intToString(do_wait) +
+            "no timeout: " + intToString(stat == boost::cv_status::no_timeout), lsWarning);
+      }
       assert(!m_ModelEvents.empty());
       event = m_ModelEvents.pop_front();
     }
@@ -893,8 +893,7 @@ namespace dss {
     for (m_ModelEvents_t::iterator it = m_ModelEvents.begin(); it != m_ModelEvents.end();) {
       if (it->getEventType() == _type) {
         it = m_ModelEvents.erase(it);
-        log("@erase : m_ModelEvents.size: " + intToString(m_ModelEvents.size()) +
-            " ct: " + intToString(++m_ModelEventsCounter), lsWarning); //< debug _taw_
+        log("@erase : m_ModelEvents.size: " + intToString(m_ModelEvents.size()), lsWarning); //< debug _taw_
       } else {
         ++it;
       }
