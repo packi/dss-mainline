44274358e9cc746c46339b9798118a9feab00cd7
Sergey 'Jin' Bostandzhyan <jin@developer.digitalstrom.org>
2015-10-30 16:57:29 +0100

    Render sensor value validity in property tree
    implements #11418

--- src/model/device.cpp
+++ src/model/device.cpp
@@ -2250,6 +2250,8 @@ namespace dss {
                 ->linkToProxy(PropertyProxyReference<int>(m_sensorInputs[m_sensorInputCount]->m_sensorType));
         entry->createProperty("index")
                 ->linkToProxy(PropertyProxyReference<int>(m_sensorInputs[m_sensorInputCount]->m_sensorIndex));
+        entry->createProperty("valid")
+                ->linkToProxy(PropertyProxyReference<bool>(m_sensorInputs[m_sensorInputCount]->m_sensorValueValidity));
         entry->createProperty("value")
                 ->linkToProxy(PropertyProxyReference<double>(m_sensorInputs[m_sensorInputCount]->m_sensorValueFloat));
         entry->createProperty("valueDS")
