bba261d9a6bf955534b32311b98275466c7c6ab5
Sergey 'Jin' Bostandzhyan <jin@developer.digitalstrom.org>
2015-10-30 16:18:50 +0100

    Fix default sensor validity setting
    As long as the sensor was not read out the valid flag should be false
    
    implements #11418

--- src/model/device.cpp
+++ src/model/device.cpp
@@ -2240,7 +2240,7 @@ namespace dss {
       binput->m_sensorValue = 0;
       binput->m_sensorValueFloat = 0;
       binput->m_sensorValueTS = DateTime::NullDate;
-      binput->m_sensorValueValidity = true;
+      binput->m_sensorValueValidity = false;
       m_sensorInputs.push_back(binput);
 
       if (m_pPropertyNode != NULL) {
