c10e116ea2870f8c866ea7a5da627bd641c4a69c
Andreas Fenkart <andreas.fenkart@dev.digitalstrom.org>
2016-08-16 13:42:49 +0200

    tests: move trigger test data to tests/data/ folder
    data/ folder should contain data that gets installed
    
    Signed-off-by: Andreas Fenkart <andreas.fenkart@dev.digitalstrom.org>

--- tests/event_trigger_tests.cpp
+++ tests/event_trigger_tests.cpp
@@ -53,13 +53,13 @@ BOOST_AUTO_TEST_SUITE(Triggers)
 
 static std::string pathSystemUser = "/system/security/users/system";
 
-const char* kDsstestsFilesDirectory = ABS_SRCDIR "/data";
+const char* kDsstestsFilesDirectory = ABS_SRCDIR "/tests/data";
 
 BOOST_FIXTURE_TEST_CASE(testSpeed, DSSInstanceFixture) {
   PropertyParser parser;
   PropertySystem &propSystem = DSS::getInstance()->getPropertySystem();
   PropertyNodePtr prop = propSystem.createProperty("/");
-  BOOST_CHECK_EQUAL(true, parser.loadFromXML(std::string(kDsstestsFilesDirectory) + std::string("/triggers.xml"), prop));
+  BOOST_CHECK_EQUAL(true, parser.loadFromXML(std::string(kDsstestsFilesDirectory) + std::string("/speed_triggers.xml"), prop));
   Security &security = DSS::getInstance()->getSecurity();
 
   {
