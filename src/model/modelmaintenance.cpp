packi/dss-mainline

commit 44b12240dc0ebcd3167dc16a3f3be1ba747295f4

ModelMaintenance: cleanup in onSensorValue

src/model/modelmaintenance.cpp
@@ -2087,21 +2087,18 @@ namespace dss {
 
       // binary input state synchronization event
       if (_sensorIndex == 32) {
-        for (int index = 0; index < pDev->getBinaryInputCount(); index++) {
-          boost::shared_ptr<State> state;
-          try {
-            state = pDev->getBinaryInput(index)->m_state;
-          } catch (ItemNotFoundException& e) {
-            continue;
-          }
+        int index = 0;
+        foreach (auto&& binaryInput, pDev->getBinaryInputs()) {
+          index++;
+          auto&& state = binaryInput->m_state;
           int oldState = state->getState();
           auto binaryInputState =
               (_sensorValue & (1 << index)) ? BinaryInputState::Active : BinaryInputState::Inactive;
           int newState = binaryInputState == BinaryInputState::Active ? State_Active : State_Inactive;
           if (newState != oldState) {
             state->setState(coSystem, newState);
-            raiseEvent(createDeviceBinaryInputEvent(pDevRev, index, // HERE
-                                                    pDev->getDeviceBinaryInputType(index),
+            raiseEvent(createDeviceBinaryInputEvent(pDevRev, index,
+                                                    binaryInput->m_inputType,
                                                     binaryInputState));
           }
         }

commit e16c7638c28d4e0ee766aeaf43c6f1619604dfe8

modelmaintenance: fix index in onSensorValue
regression from 44b12240dc0ebcd3167dc16a3f3be1ba747295f4

src/model/modelmaintenance.cpp
@@ -2089,7 +2089,6 @@ namespace dss {
       if (_sensorIndex == 32) {
         int index = 0;
         foreach (auto&& binaryInput, pDev->getBinaryInputs()) {
-          index++;
           auto&& state = binaryInput->getState();
           int oldState = state.getState();
           auto binaryInputState =
@@ -2101,6 +2100,7 @@ namespace dss {
                                                     binaryInput->m_inputType,
                                                     binaryInputState));
           }
+          index++;
         }
 
       // device status and error event