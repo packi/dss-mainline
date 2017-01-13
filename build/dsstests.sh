2a723c70a4b524c44bbe42e05e0e03c15a7fb5b3
Jean Visagie <jean.visagie@digitalstrom.com>
2017-01-13 08:38:54 +0100

    Redirect boost test-suite to error.log but keep in stdout as well, so location in test-suite.log is still correct. Error.log will be used for console output in jenkins

--- build/dsstests.sh
+++ build/dsstests.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir -p ../data/logs/ ../data/savedprops/ ../data/databases/
 
-./dsstests --catch_system_errors=no
+./dsstests --catch_system_errors=no --log_sink=stderr 2> >(tee -i error.log >&1)
 
