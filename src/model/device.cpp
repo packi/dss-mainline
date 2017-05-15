4498ebf5fd4cdeb90285a65adf81454926e64d80
Michael Tross <michael.tross@digitalstrom.com>
2017-05-15 13:43:02 +0200

    custom-action change must trigger apartmentHasChanged event
    for synchronization with backend and text2ds

--- src/model/device.cpp
+++ src/model/device.cpp
@@ -3196,6 +3196,7 @@ namespace dss {
     auto deviceReference = boost::make_shared<DeviceReference>(getDSID(), &getApartment());
     boost::shared_ptr<Event> evt = createDeviceCustomActionChangedEvent(deviceReference, id, action, title, actionParams);
     DSS::getInstance()->getEventQueue().pushEvent(evt);
+    dirty();
   }
 
   vdcapi::Message Device::getVdcProperty(
