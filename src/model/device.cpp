836f8e68b304c03aa6eca4373055340324970617
Branislav Katreniak <branislav.katreniak@digitalstrom.com>
2017-05-16 13:54:25 +0200

    device: fix -Wmissing-field-initializers
    ../src/model/device.cpp:662:79: error: missing field 'usage' initializer [-Werror,-Wmissing-field-initializers]
        DeviceSensorSpec_t sensorInputReserved1 = {SensorType::Reserved1, 0, 0, 0 };
    
    Introduced in 52ecd1406b632e43a1ff578ce029f00cf0e40d27

--- src/model/device.cpp
+++ src/model/device.cpp
@@ -659,13 +659,13 @@ namespace dss {
   } // getVendrID
 
   void Device::fillSensorTable(std::vector<DeviceSensorSpec_t>& _slist) {
-    DeviceSensorSpec_t sensorInputReserved1 = { SensorType::Reserved1, 0, 0, 0 };
-    DeviceSensorSpec_t sensorInputReserved2 = { SensorType::Reserved2, 0, 0, 0 };
-    DeviceSensorSpec_t sensorInput04 = { SensorType::ActivePower, 0, 0, 0 };
-    DeviceSensorSpec_t sensorInput05 = { SensorType::OutputCurrent, 0, 0, 0 };
-    DeviceSensorSpec_t sensorInput06 = { SensorType::ElectricMeter, 0, 0, 0 };
-    DeviceSensorSpec_t sensorInput64 = { SensorType::OutputCurrent16A, 0, 0, 0 };
-    DeviceSensorSpec_t sensorInput65 = { SensorType::ActivePowerVA, 0, 0, 0 };
+    DeviceSensorSpec_t sensorInputReserved1 = {SensorType::Reserved1, 0, 0, 0, 0, ""};
+    DeviceSensorSpec_t sensorInputReserved2 = {SensorType::Reserved2, 0, 0, 0, 0, ""};
+    DeviceSensorSpec_t sensorInput04 = {SensorType::ActivePower, 0, 0, 0, 0, ""};
+    DeviceSensorSpec_t sensorInput05 = {SensorType::OutputCurrent, 0, 0, 0, 0, ""};
+    DeviceSensorSpec_t sensorInput06 = {SensorType::ElectricMeter, 0, 0, 0, 0, ""};
+    DeviceSensorSpec_t sensorInput64 = {SensorType::OutputCurrent16A, 0, 0, 0, 0, ""};
+    DeviceSensorSpec_t sensorInput65 = {SensorType::ActivePowerVA, 0, 0, 0, 0, ""};
     DeviceClasses_t deviceClass = getDeviceClass();
     int devType = (deviceClass << 16) | m_ProductID;
 
