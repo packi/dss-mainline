aac6e48968680feebb48eada0e020dd8b2669231
Michael Tross <michael.tross@digitalstrom.com>
2017-07-12 09:17:56 +0200

    remove name and usage from sensor spec
    not part of dsmapi message

--- src/businterface.h
+++ src/businterface.h
@@ -107,8 +107,6 @@ namespace dss {
     uint32_t SensorPollInterval;
     uint8_t SensorBroadcastFlag;
     uint8_t SensorConversionFlag;
-    uint8_t usage;
-    std::string name;
   } DeviceSensorSpec_t;
 
   typedef struct DeviceSensorValue {
